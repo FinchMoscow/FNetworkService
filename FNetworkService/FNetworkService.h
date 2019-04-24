fd636b2581b547129544402526bdd69cc78ab5d1
Evgeny Orekhin <eo@finch-melrose.com>
2019-04-24 15:59:55 +0300

    Initial Commit

---
+++ FNetworkService/FNetworkService.h
@@ -0,0 +1,19 @@
+//
+//  FNetworkService.h
+//  FNetworkService
+//
+//  Created by Eugene on 24/04/2019.
+//  Copyright © 2019 Finch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FNetworkService.
+FOUNDATION_EXPORT double FNetworkServiceVersionNumber;
+
+//! Project version string for FNetworkService.
+FOUNDATION_EXPORT const unsigned char FNetworkServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FNetworkService/PublicHeader.h>
+
+
