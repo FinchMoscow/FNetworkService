65c473b74cbddf26a9be261ed39640646188cbc8
Loginov Anton <kamiraider@gmail.com>
2019-07-25 16:28:21 +0300

    add methods which will return model with http response

---
+++ FNetworkService/Supporting/ModelWithResponse.swift
@@ -0,0 +1,28 @@
+//
+//  ModelWithResponse.swift
+//  FNetworkService
+//
+//  Created by Loginov Anton on 25/07/2019.
+//  Copyright © 2019 Finch. All rights reserved.
+//
+
+import Foundation
+
+public struct ModelWithResponse<Model> {
+    
+    // MARK: - Properties
+    
+    let model: Model
+    let response: HTTPURLResponse?
+    
+    
+    // MARK: - Init
+    
+    init?(model: Model?, response: HTTPURLResponse?) {
+        
+        guard let model = model else { return nil }
+        
+        self.model = model
+        self.response = response
+    }
+}
